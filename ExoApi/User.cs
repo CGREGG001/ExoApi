e1f366d4b9c4a5fbb309743186cf219e9e85c3dc
Gregory <cgregg@belgacom.net>
2025-01-03 11:10:32 +0100

    [Add] User : constructors

--- ExoApi/User.cs
+++ ExoApi/User.cs
@@ -1,11 +1,19 @@
-using System;
-
 namespace ExoApi;
 
 public class User
 {
+
+    public User(){}
+    public User(int id, string username, string email, string password)
+    {
+        Id = id;
+        Username = username;
+        Email = email;
+        Password = password;
+    }
+
     public int Id { get; set; }
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
 }
